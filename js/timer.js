8afaa105df0d95d2051f18257ff2ebe963c8bf96
Eduardo <thiago.novak7@gmail.com>
2020-07-22 22:49:17 -0300

    Add, novas funcionalidades do pomodoro, segundo código, logica pensada de uma forma melhor

--- js/timer.js
+++ js/timer.js
@@ -1,29 +1,69 @@
 
-var rodaTime;
-function TempoAtual() {
-    var horaAtual = new Date();
-    var hora = horaAtual.getHours();
-    var minutos = horaAtual.getMinutes();
-    var segundos = horaAtual.getSeconds();
-    var mileSegundos = horaAtual.getMilliseconds();
-
-    console.log(minutos);
-
-    if(mileSegundos > 996){
-        clearInterval(rodaTime);
-        SrTempo();
-
-    }
-}
+var tempoFront = document.querySelector('.contador');
+var btnI = document.querySelector('.inicia');
+var btnP = document.querySelector('.pause');
+var btnR = document.querySelector('.reset');
+var S = 0;
+var M = 0;
+var H = 0;
 
 
+var cronometro;
+
+function relogio() {
+    // var horaAtual = new Date();
+    // var hora = horaAtual.getHours();
+    // var minutos = horaAtual.getMinutes();
+    // var segundos = horaAtual.getSeconds();
+    // var mileSegundos = horaAtual.getMilliseconds();
 
 
+    var tempoSegundo = S++;
+   
+    // var tempoHora = H++;
 
-function SrTempo() {
-    rodaTime = setInterval(function () {
-        TempoAtual();
     
-    }, 001);
+
+
+    tempoFront.innerHTML = `${0}:${M}:${tempoSegundo}`;
+
+
+
+    if (tempoSegundo >= 59) {
+        clearInterval(cronometro);
+        M += 1;
+        S = 0;
+        tempoFront.innerHTML = `${0}:${M}:${tempoSegundo}`;
+        rodandoTempo();
+    }
+
+    // if (tempoMinuto >= 59) {
+    //     tempoFront.innerHTML = `${tempoHora}:${tempoMinuto}:${tempoSegundo}`;
+    // }
+
+    
+
 }
-SrTempo();
+
+function rodandoTempo() {
+    cronometro = setInterval(function () {
+        relogio();
+    }, 1000);
+}
+
+btnI.addEventListener('click', function () {
+    rodandoTempo();
+});
+
+
+btnP.addEventListener('click', function () {
+    clearInterval(cronometro);
+});
+
+btnR.addEventListener('click', function () {
+    clearInterval(cronometro);
+    S = 0;
+    M = 0;
+    H = 0;
+    rodandoTempo();
+});
